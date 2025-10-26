730f057aa736efbf7d08c9da6efc440ff21d42c8
tochukwu7071 <tochukwubenedict03@gmail.com>
2025-10-26 01:54:17 +0100

    Add files via upload

---
+++ practice_1.cpp
@@ -0,0 +1,38 @@
+//Question 1
+
+/*#include <iostream>
+
+#include <cmath> // for sqrt() and pow() functions 
+int main(){
+    int hypo;
+    int length1;
+    int length2;
+    std::cout<< " Enter the length of the first side: ";
+    std::cin>> length1;
+    std::cout <<"Enter the length of the second side: ";
+    std::cin>> length2;
+
+    hypo = std::sqrt((length1 + length2)*(length1 + length2));
+    std::cout<<("Your hypotenuse is  ")<< hypo;
+}*/
+
+
+
+
+
+
+// GRID
+#include <iostream>
+using namespace std;
+int main(){
+
+    std::cout<< ("*************\n");
+    std::cout<< ("*             *\n");
+    std::cout<< ("*             *\n");
+    std::cout<< ("*************\n");
+    std::cout<< ("*             *\n");
+    std::cout<< ("*             *\n");
+    std::cout<< ("*************\n");
+
+    
+}
